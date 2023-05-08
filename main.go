8e3219b6bf4c7d221d010e10c97509b9296be9e9
Vasilii Perfilev <vasily.perfilyev@gmail.com>
2023-05-08 09:36:30 -0700

    Fix main

--- main.go
+++ main.go
@@ -7,7 +7,13 @@ import (
 	"github.com/vasiliiperfilev/go-app/server"
 )
 
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	playerServer := &server.PlayerServer{}
-	log.Fatal(http.ListenAndServe(":5000", playerServer))
+	server := &server.PlayerServer{Store: &InMemoryPlayerStore{}}
+	log.Fatal(http.ListenAndServe(":5000", server))
 }
