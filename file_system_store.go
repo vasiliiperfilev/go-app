94868d617e339c6bdd88b7123c16ef39ba8d59c4
Vasilii Perfilev <vasily.perfilyev@gmail.com>
2023-05-17 14:42:39 -0700

    Refactor

--- file_system_store.go
+++ file_system_store.go
@@ -39,16 +39,10 @@ func (f *FileSystemStore) RecordWin(name string) {
 }
 
 func NewFileSystemStore(file *os.File) (*FileSystemStore, error) {
-	file.Seek(0, 0)
-	info, err := file.Stat()
+	err := initialisePlayerDBFile(file)
 
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
-	}
-
-	if info.Size() == 0 {
-		file.Write([]byte("[]"))
-		file.Seek(0, 0)
+		return nil, fmt.Errorf("problem initialising player db file, %v", err)
 	}
 
 	league, err := NewLeague(file)
@@ -59,4 +53,21 @@ func NewFileSystemStore(file *os.File) (*FileSystemStore, error) {
 		database: json.NewEncoder(&tape{file}),
 		league:   league,
 	}, nil
+}
+
+func initialisePlayerDBFile(file *os.File) error {
+	file.Seek(0, 0)
+
+	info, err := file.Stat()
+
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
+	}
+
+	return nil
 }
